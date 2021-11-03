92b9609bfc01d9855f289e5cac3dfdc2f6cc5438
lucas <lucas.matw@gmail.com>
2021-11-03 17:30:02 -0300

    Optimize imports. Search ends with

--- src/main/java/ar/edu/mercadogratis/app/dao/ProductRepository.java
+++ src/main/java/ar/edu/mercadogratis/app/dao/ProductRepository.java
@@ -9,9 +9,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
-    @Query("select p from Product p where p.name like ?1% and p.status = 'ACTIVE'")
+    @Query("select p from Product p where p.name like %?1% and p.status = 'ACTIVE'")
     List<Product> searchProductByName(String name);
 
-    @Query("select p from Product p where p.name like ?1% and p.category = ?2 and p.status = 'ACTIVE'")
+    @Query("select p from Product p where p.name like %?1% and p.category = ?2 and p.status = 'ACTIVE'")
     List<Product> searchProductByNameAndCategory(String name, ProductCategory category);
 }
--- src/main/java/ar/edu/mercadogratis/app/service/ProductService.java
+++ src/main/java/ar/edu/mercadogratis/app/service/ProductService.java
@@ -1,11 +1,9 @@
 package ar.edu.mercadogratis.app.service;
 
-import ar.edu.mercadogratis.app.dao.GenericDao;
 import ar.edu.mercadogratis.app.dao.ProductRepository;
 import ar.edu.mercadogratis.app.model.Product;
 import ar.edu.mercadogratis.app.model.SearchProductRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -46,7 +44,7 @@ public class ProductService {
 
     @Transactional
     public List<Product> searchProduct(SearchProductRequest searchProductRequest) {
-        return (List<Product>) searchProductRequest.getCategory()
+        return searchProductRequest.getCategory()
                 .map(category -> productRepository.searchProductByNameAndCategory(searchProductRequest.getName(), category))
                 .orElse(productRepository.searchProductByName(searchProductRequest.getName()));
     }
